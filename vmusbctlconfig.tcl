4ea39b272cf049fce5a633ba078c1f36144c3b05
Jeromy Tompkins <tompkins@nscl.msu.edu>
2015-05-12 16:46:48 -0400

    Adds crdc2 fwload to vmusbctlconfig.tcl

--- vmusbctlconfig.tcl
+++ vmusbctlconfig.tcl
@@ -18,18 +18,26 @@ set delay(firmware) /user/s800/server/fpga/gd16.bit
 #Module config xlmTStampFw -base [expr $xlmTstampSlot<<27]
 #Module config xlmTStampFw -firmware $stamp(firmware)
 
-# Initialize the crdc firmware
-set crdc1_Slot 19 
-Module create xlm crdc1_FwLoad
-Module config crdc1_FwLoad -base [expr $crdc1_Slot<<27]
-Module config crdc1_FwLoad -firmware $crdc(firmware)
-#
-# Create the script driver that will initialize the ppacs and crdcs
-#controlscript ctlScript_ -controllertype vmusb
-#ctlScript_ configure -initscript Scripts/VM0079Init.tcl
+# Initialize the crdc 1 firmware
+#set crdc1_Slot 10 
+#Module create xlm crdc1_FwLoad
+#Module config crdc1_FwLoad -base [expr $crdc1_Slot<<27]
+#Module config crdc1_FwLoad -firmware $crdc(firmware)
 
-#Module create tcl ctlScript
-#Module config ctlScript -ensemble ctlScript_
+# Initialize the crdc 2 firmware
+set crdc2_Slot 2
+Module create xlm crdc2_FwLoad
+Module config crdc2_FwLoad -base [expr $crdc2_Slot<<27]
+Module config crdc2_FwLoad -firmware $crdc(firmware)
+
+after 1000
+
+# Create the script driver that will initialize the ppacs and crdcs
+controlscript ctlScript_ -controllertype vmusb
+ctlScript_ configure -initscript Scripts/VM0079Init.tcl
+
+Module create tcl ctlScript
+Module config ctlScript -ensemble ctlScript_
 
 
 
