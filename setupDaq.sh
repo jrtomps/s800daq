afea7581dd92d8a5eb68387e39b0e359f15335a1
s800 <s800@spdaq48.nscl.msu.edu>
2015-05-12 08:30:09 -0400

    Adds a simple env setup script

---
+++ setupDaq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if test $# -ne 1 ; then
+	echo "Usage: ./setupDaq.sh VSN"
+	echo ""
+	echo "   VSN - version of NSCLDAQ to use (i.e. 11.0-001)"
+	exit 1
+fi 
+
+unset DAQROOT
+source /usr/opt/nscldaq/$1/daqsetup.bash
+
+echo "Set the following environment variables"
+echo "DAQROOT = $DAQROOT" 
+echo "DAQBIN  = $DAQBIN" 
+echo "DAQLIB  = $DAQLIB" 
