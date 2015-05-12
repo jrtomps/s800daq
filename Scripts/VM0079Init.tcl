77d3056ae67dcbfed33e807f0ecde7f8ffdf3842
s800 <s800@spdaq48.nscl.msu.edu>
2015-05-12 09:04:48 -0400

    Move xlm72 timestamp out of VM0079Init.tcl script

--- Scripts/VM0079Init.tcl
+++ Scripts/VM0079Init.tcl
@@ -2,18 +2,19 @@
 # VMUSB controller serial number is VM0079
 
 set VMUSB VM0079
-set crdc(firmware) ~/server/fpga/crdc5v.bit
-set ppac(firmware) ~/server/fpga/ppac2v.bit
-set stamp(firmware) ~/server/fpga/stamp64.bit
-set delay(firmware) ~/server/fpga/gd16.bit
+
+set VMUSB $::Globals::aController
 
 # Create module instances if they don't exist
-if {[lsearch [itcl::find objects] stamp] == -1} {ATimeStampXLM72 stamp $VMUSB 4}
-if {[lsearch [itcl::find objects] delay] == -1} {AGD16XLM72 delay $VMUSB 7}
+
+# uses the XLMTimestamp driver instead
+#if {[lsearch [itcl::find objects] stamp] == -1} {ATimeStampXLM72 stamp $VMUSB 4}
+#if {[lsearch [itcl::find objects] delay] == -1} {AGD16XLM72 delay $VMUSB 7}
 if {[lsearch [itcl::find objects] crdc1] == -1} {ACrdcXLM72 crdc1 $VMUSB 10}
 if {[lsearch [itcl::find objects] crdc2] == -1} {ACrdcXLM72 crdc2 $VMUSB 13}
 if {[lsearch [itcl::find objects] ppac] == -1} {APpacXLM72 ppac $VMUSB 16}
-if {[lsearch [itcl::find objects] mtdc] == -1} {AMesytecMTDC32 mtdc $VMUSB 0x40000000}
+
+#if {[lsearch [itcl::find objects] mtdc] == -1} {AMesytecMTDC32 mtdc $VMUSB 0x40000000}
 
 # Configuring the XLM72V modules
 puts "Please wait. Writing firmware to crdc1 XLM72V module FPGA."
@@ -37,21 +38,21 @@ ppac Configure $ppac(firmware)
 after 500
 ppac Init ~/operations/daq/usb/Configs/s800tppacv.tcl TRACKI_PADS
 
-puts "Please wait. Writing firmware to stamp XLM72 module FPGA."
-stamp Configure $stamp(firmware)
-after 500
-stamp Init
+#puts "Please wait. Writing firmware to stamp XLM72 module FPGA."
+#stamp Configure $stamp(firmware)
+#after 500
+#stamp Init
 
-puts "Please wait. Writing firmware to delay XLM72 module FPGA."
-delay Configure $delay(firmware)
-after 500
-delay Init ~/operations/daq/usb/Configs/s800tdcdelayini.tcl TDCDELAY
-# Now start the GUI if it isn't up yet
-if {![winfo exists .gd16]} {
-	toplevel .gd16
-	delay SetupGUI .gd16
-	delay UpdateGUIGD16
-}
-
-puts "Please wait. Configuring Mesytec MTDC32. Firmware version: [mtdc GetFirmware]."
-mtdc Init
+#puts "Please wait. Writing firmware to delay XLM72 module FPGA."
+#delay Configure $VMUSB $delay(firmware)
+#after 500
+#delay Init $VMUSB ~/operations/daq/usb/Configs/s800tdcdelayini.tcl TDCDELAY
+## Now start the GUI if it isn't up yet
+#if {![winfo exists .gd16]} {
+#	toplevel .gd16
+#	delay SetupGUI .gd16
+#	delay UpdateGUIGD16
+#}
+#
+#puts "Please wait. Configuring Mesytec MTDC32. Firmware version: [mtdc GetFirmware]."
+#mtdc Init
--- vmusbctlconfig.tcl
+++ vmusbctlconfig.tcl
@@ -1,9 +1,30 @@
 
 
 package require controlscript
+package require itcl
+package crdcxlm72
+package ppacxlm72
+package timestampxlm72
 
+# Firmware locations
+set crdc(firmware) ~/server/fpga/crdc5v.bit
+set ppac(firmware) ~/server/fpga/ppac2v.bit
+set stamp(firmware) ~/server/fpga/stamp64.bit
+set delay(firmware) ~/server/fpga/gd16.bit
+
+# Initialize the timestamp with firmware
+set xlmTstampSlot 4
+Module create xlm xlmTStampFw
+Module config xlmTStampFw -base [expr $xlmTstampSlot<<27]
+Module config xlmTStampFw -firmware $tstamp(firmware)
+
+# Create the script driver that will initialize the ppacs and crdcs
 controlscript ctlScript_ -controllertype ccusb
 ctlScript_ config -initscript Scripts/VM0079Init.tcl
 
 Module create tcl ctlScript
 Module config ctlScript -ensemble ctlScript_
+
+
+
+
