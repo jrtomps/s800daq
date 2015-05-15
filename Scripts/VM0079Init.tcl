76e434aa6dd628fd80dfaab1098193500d52c229
s800 <s800@spdaq48.nscl.msu.edu>
2015-05-15 15:15:12 -0400

    Completes the ctlconfig.tcl script for VMUSBReadout

--- Scripts/VM0079Init.tcl
+++ Scripts/VM0079Init.tcl
@@ -6,20 +6,19 @@ set VMUSB $::Globals::aController
 # Create module instances if they don't exist
 
 # uses the XLMTimestamp driver instead
-#if {[lsearch [itcl::find objects] delay] == -1} {AGD16XLM72 delay $VMUSB 7}
-#if {[lsearch [itcl::find objects] crdc1] == -1} {ACrdcXLM72 crdc1 $VMUSB 10}
-if {[lsearch [itcl::find objects] crdc2] == -1} {ACrdcXLM72 crdc2 $VMUSB 2}
-#if {[lsearch [itcl::find objects] crdc2] == -1} {ACrdcXLM72 crdc2 $VMUSB 13}
-#if {[lsearch [itcl::find objects] ppac] == -1} {APpacXLM72 ppac $VMUSB 16}
+if {[lsearch [itcl::find objects] delay] == -1} {AGD16XLM72 delay $VMUSB 7}
+if {[lsearch [itcl::find objects] crdc1] == -1} {ACrdcXLM72 crdc1 $VMUSB 10}
+if {[lsearch [itcl::find objects] crdc2] == -1} {ACrdcXLM72 crdc2 $VMUSB 13}
+if {[lsearch [itcl::find objects] ppac] == -1} {APpacXLM72 ppac $VMUSB 16}
 
 #if {[lsearch [itcl::find objects] mtdc] == -1} {AMesytecMTDC32 mtdc $VMUSB 0x40000000}
 
 # Configuring the XLM72V modules
-# puts "Initializing crdc1 XLM72V"
-#crdc1 Init /user/s800/operations/daq/usb/Configs/s800crdc1v.tcl CRDC1_PADS
-#crdc1 AccessBus 1
-#for {set i 0} {$i < 100} {incr i 4} {crdc1 Write srama $i 0}
-#crdc1 ReleaseBus
+puts "Initializing crdc1 XLM72V"
+crdc1 Init /user/s800/operations/daq/usb/Configs/s800crdc1v.tcl CRDC1_PADS
+crdc1 AccessBus 1
+for {set i 0} {$i < 100} {incr i 4} {crdc1 Write srama $i 0}
+crdc1 ReleaseBus
 
 puts "Initializing crdc2 XLM72V."
 crdc2 Init /user/s800/operations/daq/usb/Configs/s800crdc2v.tcl CRDC2_PADS
@@ -27,21 +26,11 @@ crdc2 AccessBus 1
 for {set i 0} {$i < 100} {incr i 4} {crdc2 Write srama $i 0}
 crdc2 ReleaseBus
 
-#puts "Please wait. Writing firmware to ppac XLM72V module FPGA."
-#ppac Configure $ppac(firmware)
-#after 500
-#ppac Init ~/operations/daq/usb/Configs/s800tppacv.tcl TRACKI_PADS
-#
-#puts "Please wait. Writing firmware to delay XLM72 module FPGA."
-#delay Configure $VMUSB $delay(firmware)
-#after 500
-#delay Init $VMUSB ~/operations/daq/usb/Configs/s800tdcdelayini.tcl TDCDELAY
-## Now start the GUI if it isn't up yet
-#if {![winfo exists .gd16]} {
-#	toplevel .gd16
-#	delay SetupGUI .gd16
-#	delay UpdateGUIGD16
-#}
-#
+puts "Initializing ppac XLM72V"
+ppac Init /user/s800/operations/daq/usb/Configs/s800tppacv.tcl TRACKI_PADS
+
+puts "Initializing ppac XLM72V"
+delay Init /user/s800/operations/daq/usb/Configs/s800tdcdelayini.tcl TDCDELAY
+
 #puts "Please wait. Configuring Mesytec MTDC32. Firmware version: [mtdc GetFirmware]."
 #mtdc Init
--- vmusbctlconfig.tcl
+++ vmusbctlconfig.tcl
@@ -5,6 +5,7 @@ package require controlscript
 package require Itcl
 package require crdcxlm72
 package require ppacxlm72
+package require gd16xlm72
 
 # Firmware locations
 set crdc(firmware) /user/s800/server/fpga/crdc5v.bit
@@ -13,22 +14,38 @@ set stamp(firmware) /user/s800/server/fpga/stamp64.bit
 set delay(firmware) /user/s800/server/fpga/gd16.bit
 
 # Initialize the timestamp with firmware
-#set xlmTstampSlot 4
-#Module create xlm xlmTStampFw
-#Module config xlmTStampFw -base [expr $xlmTstampSlot<<27]
-#Module config xlmTStampFw -firmware $stamp(firmware)
+set xlmTstampSlot 4
+Module create xlm xlmTStampFw
+Module config xlmTStampFw -base [expr $xlmTstampSlot<<27]
+Module config xlmTStampFw -firmware $stamp(firmware)
+
+# Initialize the delay firmware
+set delay_Slot 7 
+Module create xlm delay_FwLoad
+Module config delay_FwLoad -base [expr $delay_Slot<<27]
+Module config delay_FwLoad -firmware $delay(firmware)
+Module config delay_FwLoad -validate on -signature [expr 0xdaba0006]
 
 # Initialize the crdc 1 firmware
-#set crdc1_Slot 10 
-#Module create xlm crdc1_FwLoad
-#Module config crdc1_FwLoad -base [expr $crdc1_Slot<<27]
-#Module config crdc1_FwLoad -firmware $crdc(firmware)
+set crdc1_Slot 10 
+Module create xlm crdc1_FwLoad
+Module config crdc1_FwLoad -base [expr $crdc1_Slot<<27]
+Module config crdc1_FwLoad -firmware $crdc(firmware)
+Module config crdc1_FwLoad -validate on -signature [expr 0xdaba0009]
 
 # Initialize the crdc 2 firmware
-set crdc2_Slot 2
+set crdc2_Slot 13
 Module create xlm crdc2_FwLoad
 Module config crdc2_FwLoad -base [expr $crdc2_Slot<<27]
 Module config crdc2_FwLoad -firmware $crdc(firmware)
+Module config crdc2_FwLoad -validate on -signature [expr 0xdaba0009]
+
+# Initialize the ppac firmware
+set ppac_Slot 16
+Module create xlm ppac_FwLoad
+Module config ppac_FwLoad -base [expr $ppac_Slot<<27]
+Module config ppac_FwLoad -firmware $ppac(firmware)
+Module config ppac_FwLoad -validate on -signature [expr 0xdaba0005]
 
 # Create the script driver that will initialize the ppacs and crdcs
 controlscript ctlScript_ -controllertype vmusb
