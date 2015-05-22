a3157cb0434f31d27c9dadca888787bab122d3bf
s800 <s800@spdaq48.nscl.msu.edu>
2015-05-22 16:56:34 -0400

    Adds some formatting to printed outptu

--- Scripts/VM0079Init.tcl
+++ Scripts/VM0079Init.tcl
@@ -1,6 +1,8 @@
 # Init sequence for s800 DAQ
 # VMUSB controller serial number is VM0079
 
+puts "Processing VM0079Init.tcl"
+
 set VMUSB $::Globals::aController
 
 # Create module instances if they don't exist
@@ -14,21 +16,22 @@ if {[lsearch [itcl::find objects] ppac] == -1} {APpacXLM72 ppac $VMUSB 16}
 #if {[lsearch [itcl::find objects] mtdc] == -1} {AMesytecMTDC32 mtdc $VMUSB 0x40000000}
 
 # Configuring the XLM72V modules
-puts "Initializing crdc1 XLM72V"
+puts "\tInitializing crdc1 XLM72V"
 crdc1 Init /user/s800/operations/daq/usb/Configs/s800crdc1v.tcl CRDC1_PADS
 crdc1 AccessBus 1
 for {set i 0} {$i < 100} {incr i 4} {crdc1 Write srama $i 0}
 crdc1 ReleaseBus
 
-puts "Initializing crdc2 XLM72V."
+puts "\tInitializing crdc2 XLM72V."
 crdc2 Init /user/s800/operations/daq/usb/Configs/s800crdc2v.tcl CRDC2_PADS
 crdc2 AccessBus 1
 for {set i 0} {$i < 100} {incr i 4} {crdc2 Write srama $i 0}
 crdc2 ReleaseBus
 
-puts "Initializing ppac XLM72V"
+puts "\tInitializing ppac XLM72V"
 ppac Init /user/s800/operations/daq/usb/Configs/s800tppacv.tcl TRACKI_PADS
 
-puts "Initializing delay XLM72V"
+puts "\tInitializing delay XLM72V"
 delayBox Init /user/s800/operations/daq/usb/Configs/s800tdcdelayini.tcl TDCDELAY
 
+puts "Done"
