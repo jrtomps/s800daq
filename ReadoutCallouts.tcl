f1d1fb64f86ec691f95fe67cc6f80aa6e6b26f04
s800 <s800@spdaq48.nscl.msu.edu>
2015-05-22 10:58:06 -0400

    Uncomments ReadoutCallouts.tcl

--- ReadoutCallouts.tcl
+++ ReadoutCallouts.tcl
@@ -1,11 +1,11 @@
-#
-#package require evbcallouts
-#
-#::EVBC::useEventBuilder
-#
-#::EVBC::registerRingSource tcp://spdaq48/rawccusb libccusbtstamp.so 0  "S800 CAMAC"
-#::EVBC::registerRingSource tcp://spdaq48/rawvmusb libvmusbtstamp.so 1  "S800 VMUSB"
-#
-#proc OnStart {} {
-#::EVBC::initialize -restart false -glombuild true -glomdt 10 -destring s800built
-#}
+
+package require evbcallouts
+
+::EVBC::useEventBuilder
+
+::EVBC::registerRingSource tcp://spdaq48/rawccusb libdummytstamp.so 0  "CCUSB"
+::EVBC::registerRingSource tcp://spdaq48/rawvmusb libdummytstamp.so 1  "VMUSB"
+
+proc OnStart {} {
+::EVBC::initialize -restart false -glombuild true -glomdt 10 -destring s800built
+}
